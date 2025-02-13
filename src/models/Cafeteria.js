a6bddc4dc4c6153daa22af43751d747eed864455
maria parma <mariaparma.tds1.senai@gmail.com>
2025-02-13 09:35:01 -0300

    commit

---
+++ src/models/Cafeteria.js
@@ -0,0 +1,10 @@
+class Cafeteria {
+    constructor(id, , autor, ano) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+    }
+}
+
+module.exports = Livro;
